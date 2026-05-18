f3375cdad2453b9eca2313a00cc58439f3dff6cf
root <root@pz-x9.localdomain>
2026-05-18 19:26:53 +1200

    Add recursive docs build with basepath support

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
 
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for entry in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, entry)
         to_path = os.path.join(dest_dir_path, entry)
@@ -21,22 +24,23 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
                     from_path,
                     template_path,
                     to_path[:-3] + ".html",
+                    basepath,
                 )
         else:
             os.makedirs(to_path, exist_ok=True)
-            generate_pages_recursive(from_path, template_path, to_path)
+            generate_pages_recursive(from_path, template_path, to_path, basepath)
 
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
-    print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    print("Copying static files to docs directory...")
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
 
 
 main()
