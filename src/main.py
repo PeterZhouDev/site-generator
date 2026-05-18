a0dc594b533be9cfced3f64f7fd4430a419cd457
root <root@pz-x9.localdomain>
2026-05-18 19:57:13 +1200

    soln

--- src/main.py
+++ src/main.py
@@ -3,44 +3,30 @@ import shutil
 import sys
 
 from copystatic import copy_files_recursive
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
+
 
 dir_path_static = "./static"
-dir_path_docs = "./docs"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
-basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-
-
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
-    for entry in os.listdir(dir_path_content):
-        from_path = os.path.join(dir_path_content, entry)
-        to_path = os.path.join(dest_dir_path, entry)
-
-        if os.path.isfile(from_path):
-            if from_path.endswith(".md"):
-                generate_page(
-                    from_path,
-                    template_path,
-                    to_path[:-3] + ".html",
-                    basepath,
-                )
-        else:
-            os.makedirs(to_path, exist_ok=True)
-            generate_pages_recursive(from_path, template_path, to_path, basepath)
+default_basepath = "/"
 
 
 def main():
-    print("Deleting docs directory...")
-    if os.path.exists(dir_path_docs):
-        shutil.rmtree(dir_path_docs)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
 
-    print("Copying static files to docs directory...")
-    copy_files_recursive(dir_path_static, dir_path_docs)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print("Generating pages...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_docs, basepath)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
